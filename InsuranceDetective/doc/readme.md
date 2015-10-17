3050d4b2802a5b5ef8325e574eb830314f374fa4
Tanner Stefan <tanns2@bfh.ch>
2015-10-17 15:31:00 +0200

    Added first structure

---
+++ InsuranceDetective/doc/readme.md
@@ -0,0 +1,3 @@
+### Doc
+
+Put all your important documents here - link them additionally in this file and describe them carefully.
---
+++ InsuranceDetective/src/Info.java
@@ -0,0 +1,9 @@
+
+public class Info {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Repository for Project 1 - Insurance Detective");
+	}
+
+}
